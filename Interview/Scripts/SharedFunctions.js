6ea137f053167c83742264d1c69cbe397c8b2bdd
aram.avetisyan <aram.avetisyan@margasoft.com>
2015-11-30 18:55:27 +0400

    Update

---
+++ Interview/Scripts/SharedFunctions.js
@@ -0,0 +1,31 @@
+﻿
+$(document).ready(function () {
+    $("#login").click(function () {
+        var obj = {
+            Email: $('input[name="Email"]').val(),
+            Password: $('input[name="Password"]').val()
+        }
+        Login(obj);
+    });
+});
+
+
+function Login(model) {
+    $.ajax({
+        url: "/Home/Login",
+        type: "POST",
+        data: model,
+        success: function (data) {
+            if (data === 'True') {
+                $('#loginsuccess').show();
+            } else {
+                alert("Your password is wrong.");
+            }
+        },
+        error: function () {
+            alert("Your password is wrong.");
+        }
+    });
+  
+}
+
